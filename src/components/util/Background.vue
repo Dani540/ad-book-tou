ae938efdc7442b222f4f2169fe7f28b1d739bdfb
Dani <daniel.palma.1345@gmail.com>
2024-09-30 14:03:14 -0300

    [Component] - Background Image Container - Initial Project Version

---
+++ src/components/util/Background.vue
@@ -0,0 +1,34 @@
+<!--
+/**
+* Background component
+* This component serves as a fixed background image for the application.
+*/
+-->
+
+<script setup>
+
+</script>
+
+<template>
+  <div class="background">
+    <!-- Background image -->
+    <img alt="Background_v2" src="../../assets/Background_v2.png" />
+  </div>
+</template>
+
+<style scoped>
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -10;
+  width: 100%;
+  height: 100%;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
