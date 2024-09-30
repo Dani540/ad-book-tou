acc5f7ba5c24441e2d2110b9b343fce6abc9d187
Dani <daniel.palma.1345@gmail.com>
2024-09-30 14:05:54 -0300

    [Component] - HomeView of App (Index) - Initial Project Version

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<!--
+/**
+* HomeView component
+* This component serves as the main view displaying the header and card display.
+*/
+-->
+
+<script setup>
+import CardDisplay from "@/components/central/CardDisplay.vue";
+import Upper from "@/components/upper_context/Upper.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <!-- Upper component for the header -->
+      <Upper />
+    </div>
+  </header>
+  <main>
+    <div class="wrapper">
+      <!-- CardDisplay component for displaying cards -->
+      <CardDisplay />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
